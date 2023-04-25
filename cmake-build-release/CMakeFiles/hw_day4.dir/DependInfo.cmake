d706a4eec87453c0e2ca31914f8f5cb9e4128f3c
elfsa-code <elfsaikou@gmail.com>
2023-04-25 23:31:25 +0800

    first commit

---
+++ cmake-build-release/CMakeFiles/hw_day4.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/elfsa/hw_day4/src/apps/hw_day4.cpp" "/home/elfsa/hw_day4/cmake-build-release/CMakeFiles/hw_day4.dir/src/apps/hw_day4.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "include"
+  "../include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/elfsa/hw_day4/cmake-build-release/CMakeFiles/ORB.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
